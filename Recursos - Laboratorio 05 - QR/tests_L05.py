a9311a79d674fd1903574edd4ed286b43946f638
nazolaz <Nazareno_Olaz@hotmail.com>
2025-09-29 16:16:30 -0300

    Impl de QR con GS

--- Recursos - Laboratorio 05 - QR/tests_L05.py
+++ Recursos - Laboratorio 05 - QR/tests_L05.py
@@ -1,12 +1,90 @@
 ### Funciones L05-QR
+
+def norma (x, p):
+    if p == 'inf':
+        return max(map(abs ,x))
+    
+    res = 0 
+    for xi in x:
+        res += xi**p
+    return res**(1/p)
+
+
+def normaliza(Xs, p):
+    res = []
+    for x in Xs:
+        res.append(x/norma(x,p))
+    return res
+
+def cantFilas(A):
+    return len(A)
+
+def cantColumnas(A):
+    return len(A[0])
+
+def conseguirColumna(A, j):
+    columna = np.zeros(cantColumnas(A))
+    for k in range(cantColumnas(A)):
+        columna.append(A[k][0])
+
+    return np.array(columna)
+
+def insertarColumna(A, b, j):
+    for k in range(cantColumnas(A)):
+        A[k][j] = b[k]
+    return A
+
+def productoInterno(u, v):
+    sum = 0
+    for ui, vi in zip(u, v):
+        sum += ui*vi
+    
+    return sum
+
+def productoEscalar(u, k):
+    res = ()
+    for _, ui in enumerate(u):
+        res.append(k*ui)
+    
+    return res
+
+def restaVectorial(u, v):
+    res = ()
+    for ui, vi in zip(u,v):
+        res.append(ui - vi)
+
+    return res
+
 def QR_con_GS(A,tol=1e-12,retorna_nops=False):
-    """
-    A una matriz de n x n 
-    tol la tolerancia con la que se filtran elementos nulos en R
-    retorna_nops permite (opcionalmente) retornar el numero de operaciones realizado
-    retorna matrices Q y R calculadas con Gram Schmidt (y como tercer argumento opcional, el numero de operaciones).
-    Si la matriz A no es de n x n, debe retornar None
-    """
+    Q = np.zeros(cantFilas(A),cantColumnas(A))
+    R = np.zeros(cantFilas(A),cantColumnas(A))
+    nops = 0
+
+    a_1 = conseguirColumna(A, 0)
+    insertarColumna(Q, normaliza(a_1, 2), 0)
+    R[0][0] = norma(a_1, 2)
+
+    for j in range(1, cantFilas(A)):
+        qMoño_j = conseguirColumna(A, j)
+
+        for k in range(0, j-1):
+            q_k = conseguirColumna(Q, k)
+            R[k][j] = productoInterno(q_k, qMoño_j)
+            qMoño_j = restaVectorial(qMoño_j, productoEscalar(q_k, R[k][j]))
+        
+        R[j][j] = norma(qMoño_j, 2)
+        insertarColumna(Q, productoEscalar(qMoño_j, 1/R[j][j]), j)
+
+    if (retorna_nops):
+        return Q, R, nops
+
+    else:
+        return Q, R
+
+
+
+
+
 
 def QR_con_HH(A,tol=1e-12):
     """
