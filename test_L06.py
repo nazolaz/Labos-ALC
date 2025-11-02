09be846845b6853a2168ef92a83dec381f202a1f
nazolaz <Nazareno_Olaz@hotmail.com>
2025-11-02 04:04:44 -0300

    chupete

--- test_L06.py
+++ test_L06.py
@@ -6,23 +6,30 @@ from modulo.moduloALCaux import*
 
 def diagRH(A, tol = 1e-15, K = 1000):
     n = len(A)
-    v1, l1, _ = metpot2k(A, tol, K)
-    resta = normalizarVector(restaVectorial(colCanonico(n,0), v1),2)
-    matrizResta = productoExterno(resta, traspuesta(resta))
-    Hv1 = restaMatricial(nIdentidad(n), productoEscalar(matrizResta, 2))
-    mid = productoMatricial(Hv1,productoMatricial(A,traspuesta(Hv1)))
+    v1, l1, kk = metpot2k(A, tol, K)
+    #print(kk)
+    # resta = normalizarVector(restaVectorial(colCanonico(n,0), v1),2)
+    # matrizResta = productoExterno(resta, traspuesta(resta))
+    # Hv1 = restaMatricial(nIdentidad(n), productoEscalar(matrizResta, 2))
+    # mid = productoMatricial(Hv1,productoMatricial(A,traspuesta(Hv1)))
+    resta = colCanonico(n,0) - v1
+    norma = np.linalg.norm(resta)
+    restaNormalizada = resta / norma
+    matrizResta = restaNormalizada @ restaNormalizada.T
+    Hv1 = np.eye(n) - np.multiply(matrizResta, 2)
+    mid = Hv1 @ A @ Hv1.T
 
     if n == 2:
         return Hv1, mid
-    
     
     Amoño = submatriz(mid, 2, n)
     Smoño, Dmoño = diagRH(Amoño, tol, K)
     D = extenderConIdentidad(Dmoño, n)
     D[0][0] = l1
-    S = productoMatricial(Hv1, extenderConIdentidad(Smoño, n))
+    S = Hv1 @ extenderConIdentidad(Smoño, n)
     return S, D
 
+#
 
 # Tests diagRH
 D = np.diag([1,0.5,0.25])
@@ -33,7 +40,9 @@ S = np.vstack([
               ]).T
 
 A = S@D@S.T
+print(D)
 SRH,DRH = diagRH(A,tol=1e-15,K=1e5)
+print(DRH)
 assert np.allclose(D,DRH)
 assert np.allclose(np.abs(S.T@SRH),np.eye(A.shape[0]),atol=1e-7)
 
@@ -42,10 +51,9 @@ exitos = 0
 for i in range(100):
     A = np.random.random((5,5))
     A = 0.5*(A+A.T)
-    S,D = diagRH(A,tol=1e-15,K=1e5)
+    S,D = diagRH(A,tol=1e-15,K=1e7)
     ARH = S@D@S.T
     e = normaExacta(ARH-A,p='inf')
-    print(e)
     if e < 1e-5: 
         exitos += 1
 assert exitos >= 95
